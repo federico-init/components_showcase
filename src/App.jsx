e01f9607c6f2857b31fbceda5eee17aff2f25ae1
federico-init <fefe.borrometi@gmail.com>
2023-10-31 20:11:20 +0100

    chore: installed propTypes lib and added check on button components props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+// components
+import Button from "./Button";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button>Click me!</Button>
+      </div>
+      <div>
+        <Button>Buy Now!</Button>
+      </div>
+      <div>
+        <Button>See Deal!</Button>
+      </div>
+      <div>
+        <Button>Hide Ads!</Button>
+      </div>
+      <div>
+        <Button>Whatever!</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
