8a6904d2a5e5c3f8762a5c338f74d03e13d324ba
federico-init <fefe.borrometi@gmail.com>
2023-11-07 15:32:37 +0100

    chore: added show/hide accordion functionality

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react";
 
+// React icons
+import { GoChevronDown, GoChevronRight } from "react-icons/go";
+
 const Accordion = ({ items }) => {
   // this state refers to the expanded section's id
-  const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(null);
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
+    const chevronIcon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronRight />}</span>
+    );
+
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
+        <div
+          onClick={
+            isExpanded
+              ? () => setExpandedIndex(null)
+              : () => setExpandedIndex(index)
+          }
+        >
+          {item.label}
+          {chevronIcon}
+        </div>
+
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
