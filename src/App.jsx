97bce34678a27f1e24ec8757c253fd63a4488498
federico-init <fefe.borrometi@gmail.com>
2023-11-07 10:24:36 +0100

    chore: accordion component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,25 @@
 // components
-import Button from "./Button";
-
-// React icons
-import { GoBell } from "react-icons/go";
+import Accordion from "./components/Accordion";
 
 function App() {
-  return (
-    <div>
-      <div>
-        <Button success rounded outline className="mb-1">
-          Click me!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoBell />
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Whatever!
-        </Button>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      id: 1,
+      label: "Can I use React on a project?",
+      content: "Yes you can.",
+    },
+    {
+      id: 2,
+      label: "Can I use Javascript on a project?",
+      content: "Yes you can.",
+    },
+    {
+      id: 3,
+      label: "Can I use CSS on a project?",
+      content: "Yes you can.",
+    },
+  ];
+  return <Accordion items={items} />;
 }
 
 export default App;
---
+++ src/components/pages/ButtonPage.jsx
@@ -0,0 +1,40 @@
+// components
+import Button from "./components/Button";
+
+// React icons
+import { GoBell } from "react-icons/go";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button success rounded outline className="mb-1">
+          Click me!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoBell />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Whatever!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
