5aa703fce55683bcd0218887b1382afff6dc848c
federico-init <fefe.borrometi@gmail.com>
2024-04-21 00:34:25 +0200

    feat(Modal): handle scrolling while modal is open

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,24 @@
 import ReactDOM from "react-dom";
+import { useEffect } from "react";
 
 const Modal = ({ children, actionBar, onClose }) => {
+  // I'm adding a class to the body to prevent scrolling when the modal is open
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
+
   // I'm creating a ReactDOM portal here to make sure the rendered modal is not affected by the parent's CSS
   return ReactDOM.createPortal(
     <div>
       <div
         onClick={onClose}
-        className="absolute inset-0 bg-gray-300 opacity-80"
+        className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">
+      <div className="fixed inset-40 p-10 bg-white">
         <div className="flex flex-col justify-between h-full">
           {children}
           <div className="flex justify-end">{actionBar}</div>
